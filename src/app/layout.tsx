8df852add8a686e7a1db3af5ca7ef96f3c3aea18
Yearthmain <yearthmain@gmail.com>
2025-03-19 23:45:23 +0800

    chore: 移除 .env 文件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
